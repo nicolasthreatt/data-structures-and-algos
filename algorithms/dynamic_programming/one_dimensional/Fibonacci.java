798ab6b26500ee38f65628f03aa6e70327240d4c
Nicolas Threatt <nicolasthreattcu@gmail.com>
2025-11-17 17:34:35 -0600

    Dynammic Programming Review - fibonacci

---
+++ algorithms/dynamic_programming/one_dimensional/Fibonacci.java
@@ -0,0 +1,80 @@
+/*
+Fibonacci Number
+https://leetcode.com/problems/fibonacci-number/
+      
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+such that each number is the sum of the two preceding ones, starting from 0 and 1.
+
+That is,
+    F(0) = 0, F(1) = 1
+    F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+Given n, calculate F(n).
+
+Example 1:
+    Input: n = 2
+    Output: 1
+    Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+    Input: n = 3
+    Output: 2
+    Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Example 3:
+    Input: n = 4
+    Output: 3
+    Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+Constraints:
+    * 0 <= n <= 30
+*/
+package algorithms.dynamic_programming.one_dimensional;
+
+public class Fibonacci {
+
+    // Algorithm(s) Used: Recursion
+    // Time Complexity: O(2^n)
+    // Space Complexity: O(n)
+    public int fibonacciI(int n) {
+        if (n <= 1) return n;
+
+        return fibonacciI(n - 1) + fibonacciI(n - 2);
+    }
+
+    // Algorithm(s) Used: Dynamic Programming (1-D), Bottom-Up
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    public int fibonacciII(int n) {
+        if (n <= 1) return n;
+
+        int[] fib = new int[n];
+
+        for (int i = 0; i < n + 1; i++) {
+            if (i <= 1) {
+                fib[i] = 0;
+            } else {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+        }
+
+        return fib[n];
+    }
+
+    // Algorithm(s) Used: Dynamic Programming (1-D), Bottom-Up
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    public int fibonacciIII(int n) {
+        if (n <= 1) return n;
+
+        int prev = 0, curr = 0;
+
+        for (int i = 2; i < n + 1; i++) {
+            int tmp = prev;
+            prev = curr;
+            curr = tmp + curr;
+        }
+
+        return curr;
+    }
+}
--- algorithms/dynamic_programming/one_dimensional/fibonacci.py
+++ algorithms/dynamic_programming/one_dimensional/fibonacci.py
@@ -1,29 +1,52 @@
 """
-Fibonacci
-    - https://en.wikipedia.org/wiki/Fibonacci_number
-    - In mathematics, the Fibonacci numbers, commonly denoted F(n), form a sequence,
-      the Fibonacci sequence, in which EACH NUMBER IS THE SUM OF THE TWO PRECEDING ONES
+Fibonacci Number
+https://leetcode.com/problems/fibonacci-number/
       
-Example:
-    Input: n = 5
-    Output: 5
-    Explanation:
-        - The first five Fibonacci numbers are: 0, 1, 1, 2, 3, 5.
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+such that each number is the sum of the two preceding ones, starting from 0 and 1.
+
+That is,
+    F(0) = 0, F(1) = 1
+    F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+Given n, calculate F(n).
+
+Example 1:
+    Input: n = 2
+    Output: 1
+    Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+    Input: n = 3
+    Output: 2
+    Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Example 3:
+    Input: n = 4
+    Output: 3
+    Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+Constraints:
+    * 0 <= n <= 30
 """
+
+# Algorithm Used: Recursion
+# Time Complexity: O(2^n)
+# Space Complexity: O(n)
+def fibonacciI(n: int) -> int:
+    if n <= 1:
+        return n
+
+    return fibonacciI(n - 1) + fibonacciI(n - 2)
 
 
 # Algorithm Used: Dynamic Programming (1-D), Bottom-Up
 # Time Complexity: O(n)
 # Space Complexity: O(n)
-def fibonacci(n: int) -> int:
-    """Return the nth Fibonacci number.
+def fibonacciII(n: int) -> int:
+    if n <= 1:
+        return n
 
-    Args:
-        n (int): The nth Fibonacci number to return.
-
-    Returns:
-        int: The nth Fibonacci number.
-    """
     fib = [0, 1]  # Base cases
 
     # For each number from 2 to n + 1, compute the nth Fibonacci number.
@@ -34,3 +57,42 @@ def fibonacci(n: int) -> int:
         fib.append(fib[i - 1] + fib[i - 2])  # Memoization
 
     return fib[n]
+
+
+# Algorithm Used: Dynamic Programming (1-D), Bottom-Up
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def fibonacciIII(n: int) -> int:
+    if n <= 1:
+        return n
+
+    prev, curr = 0 , 1
+    for _ in range(2, n + 1):
+        # prev, curr = curr, prev + curr
+        tmp = prev
+        prev = curr
+        curr = tmp + curr
+
+    return curr
+
+
+if __name__ == "__main__":
+    test_cases = [
+        (0, 0),
+        (1, 1),
+        (2, 1),
+        (3, 2),
+        (4, 3),
+        (5, 5),
+        (6, 8),
+        (7, 13),
+        (10, 55),
+        (15, 610),
+        (20, 6765),
+        (30, 832040),
+    ]
+
+    for func in [fibonacciI, fibonacciII, fibonacciIII]:
+        for n, expected in test_cases:
+            result = func(n)
+            assert result == expected
