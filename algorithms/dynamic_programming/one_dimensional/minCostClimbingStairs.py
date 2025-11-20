b3ceea59a237c33a301c503015e627b7576c5fc8
Nicolas Threatt <nicolasthreattcu@gmail.com>
2025-11-19 22:40:20 -0600

    Dynamic Programming Review

---
+++ algorithms/dynamic_programming/one_dimensional/minCostClimbingStairs.py
@@ -0,0 +1,68 @@
+"""
+Min Cost Climbing Stairs
+https://leetcode.com/problems/min-cost-climbing-stairs/
+
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase.
+Once you pay the cost, you can either climb one or two steps.
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+Example 1:
+    Input: cost = [10,15,20]
+    Output: 15
+    Explanation:
+        You will start at index 1.
+            - Pay 15 and climb two steps to reach the top.
+        The total cost is 15.
+
+Example 2:
+    Input: cost = [1,100,1,1,1,100,1,1,100,1]
+    Output: 6
+    Explanation:
+        You will start at index 0.
+        - Pay 1 and climb two steps to reach index 2.
+        - Pay 1 and climb two steps to reach index 4.
+        - Pay 1 and climb two steps to reach index 6.
+        - Pay 1 and climb one step to reach index 7.
+        - Pay 1 and climb two steps to reach index 9.
+        - Pay 1 and climb one step to reach the top.
+    The total cost is 6.
+
+Constraints:
+    * 2 <= cost.length <= 1000
+    * 0 <= cost[i] <= 999
+"""
+from typing import List
+
+
+# Algorithm(s) Used: Dynamic Prgramming (1-D), Bottom-Up
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def minCostClimbingStairs(cost: List[int]) -> int:
+    one_step_cost, two_steps_cost = cost[0], cost[1]
+
+    if len(cost) <= 2:
+        return min(one_step_cost, two_steps_cost)
+
+    for i in range(2, len(cost)):
+        curr_cost = cost[i] + min(one_step_cost, two_steps_cost)
+        one_step_cost = two_steps_cost
+        two_steps_cost = curr_cost
+    
+    return min(one_step_cost, two_steps_cost)
+
+
+if __name__ == "__main__":
+    test_cases = [
+        ([10, 15, 20], 15),
+        ([1, 100, 1, 1, 1, 100, 1, 1, 100, 1], 6),
+        ([0, 0], 0),
+        ([1, 2, 3, 4], 4),
+        ([2, 2, 2, 2, 2], 4),
+    ]
+
+    for func in [minCostClimbingStairs,]:
+        for cost, expected in test_cases:
+            result = func(cost)
+            assert result == expected
