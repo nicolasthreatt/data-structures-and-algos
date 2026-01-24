16a1985ba52587d8315a563501c79b30b8b5794a
Nicolas Threatt <nicolasthreattcu@gmail.com>
2026-01-23 19:34:29 -0600

    Design - Parking System

---
+++ design/ParkingSystem.py
@@ -0,0 +1,73 @@
+"""
+Desgin Parking System
+https://leetcode.com/problems/design-parking-system
+
+Design a parking system for a parking lot.
+The parking lot has three kinds of parking spaces with a fixed number of slots for each size:
+    - big, medium, and small
+
+Implement the ParkingSystem class:
+    - ParkingSystem(int big, int medium, int small)
+        + Initializes object of the ParkingSystem class.
+        + The number of slots for each parking space are given as part of the constructor.
+    - bool addCar(int carType)
+        + Checks whether there is a parking space of carType for the car that wants to get into the parking lot.
+          carType can be of three kinds: big, medium, or small, which are represented by 1, 2, and 3 respectively.
+          A car can only park in a parking space of its carType.
+          If there is no space available, return false, else park the car in that size space and return true.
+
+Example
+    - Input:
+        ["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+        [[1, 1, 0], [1], [2], [3], [1]]
+    - Output:
+        [null, true, true, false, false]
+    Explanation
+        ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+        parkingSystem.addCar(1); // return true because there is 1 available slot for a big car
+        parkingSystem.addCar(2); // return true because there is 1 available slot for a medium car
+        parkingSystem.addCar(3); // return false because there is no available slot for a small car
+        parkingSystem.addCar(1); // return false because there is no available slot for a big car. It is already occupied.
+
+Constraints:
+    - 0 <= big, medium, small <= 1000
+    - carType is 1, 2, or 3
+    - At most 1000 calls will be made to addCar
+"""
+
+
+# Algorithm Used: Array
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        self.spaces = [0, big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if self.spaces[carType]:
+            self.spaces[carType] -= 1
+            return True
+ 
+        return False
+
+
+if __name__ == "__main__":
+    parking_system = ParkingSystem(1, 1, 0)
+    assert parking_system.addCar(1) is True
+    assert parking_system.addCar(2) is True
+    assert parking_system.addCar(3) is False
+    assert parking_system.addCar(1) is False
+    assert parking_system.addCar(2) is False
+
+    parking_system = ParkingSystem(2, 0, 1)
+    assert parking_system.addCar(1) is True
+    assert parking_system.addCar(1) is True
+    assert parking_system.addCar(1) is False
+    assert parking_system.addCar(2) is False
+    assert parking_system.addCar(3) is True
+    assert parking_system.addCar(3) is False
+
+    parking_system = ParkingSystem(0, 0, 0)
+    assert parking_system.addCar(1) is False
+    assert parking_system.addCar(2) is False
+    assert parking_system.addCar(3) is False
